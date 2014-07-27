fc91bf3527670760741670c7fd8b93208d1c6ed2
David Fowler <davidfowl@gmail.com>
2014-07-26 22:20:22 -0700

    Fixup the F# support and changed configurations to frameworks

--- src/FSharpSupport/FSharpProjectReference.cs
+++ src/FSharpSupport/FSharpProjectReference.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Versioning;
+using System.Text;
 using Microsoft.Framework.Runtime;
 
 namespace FSharpSupport
@@ -44,27 +46,212 @@ namespace FSharpSupport
 
         public Assembly Load(IAssemblyLoaderEngine loaderEngine)
         {
-            throw new NotImplementedException();
+            string outputDir = Path.Combine(Path.GetTempPath(), "dynamic-assemblies");
+
+            var result = Emit(outputDir, emitPdb: true, emitDocFile: false);
+
+            if (!result.Success)
+            {
+                throw new CompilationException(result.Errors.ToList());
+            }
+
+            var assemblyPath = Path.Combine(outputDir, _project.Name + ".exe");
+
+            return loaderEngine.LoadFile(assemblyPath);
         }
 
         public IProjectBuildResult EmitAssembly(string outputPath)
         {
-            throw new NotImplementedException();
+            return Emit(outputPath, emitPdb: true, emitDocFile: true);
         }
 
         public void EmitReferenceAssembly(Stream stream)
         {
-            throw new NotImplementedException();
+            string outputDir = Path.Combine(Path.GetTempPath(), "reference-assembly-" + Guid.NewGuid().ToString());
+
+            try
+            {
+                var result = Emit(outputDir, emitPdb: false, emitDocFile: false);
+
+                if (!result.Success)
+                {
+                    return;
+                }
+
+                using (var fs = File.OpenRead(Path.Combine(outputDir, _project.Name + ".dll")))
+                {
+                    fs.CopyToAsync(stream);
+                }
+            }
+            finally
+            {
+                Directory.Delete(outputDir, true);
+            }
         }
 
         public IProjectBuildResult GetDiagnostics()
         {
-            return new ProjectBuildResult(success: false, warnings: Enumerable.Empty<string>(), errors: Enumerable.Empty<string>());
+            string outputDir = Path.Combine(Path.GetTempPath(), "diagnostics-" + Guid.NewGuid().ToString());
+
+            try
+            {
+                return Emit(outputDir, emitPdb: false, emitDocFile: false);
+            }
+            finally
+            {
+                Directory.Delete(outputDir, true);
+            }
         }
 
         public IList<ISourceReference> GetSources()
         {
-            return new List<ISourceReference>();
+            return _project.SourceFiles.Select(p => (ISourceReference)new SourceFileReference(p))
+                                       .ToList();
+        }
+
+        public IProjectBuildResult Emit(string outputPath, bool emitPdb, bool emitDocFile, bool emitExe = false)
+        {
+            var tempBasePath = Path.Combine(outputPath, _project.Name, "obj");
+            var outputDll = Path.Combine(outputPath, _project.Name + (emitExe ? ".exe" : ".dll"));
+
+            // csc /out:foo.dll / target:library Program.cs
+            var fscArgBuilder = new StringBuilder()
+                         .Append("--noframework ")
+                         .Append("--nologo ")
+                         .AppendFormat(@"--out:""{0}""", outputDll)
+                         .Append(" ")
+                         .AppendFormat("--target:{0} ", emitExe ? "exe" : "dll");
+
+            Directory.CreateDirectory(tempBasePath);
+
+            if (emitPdb)
+            {
+                var pdb = Path.Combine(outputPath, _project.Name + ".pdb");
+
+                fscArgBuilder = fscArgBuilder
+                    .Append("--debug ")
+                    .AppendFormat(@"--pdb:""{0}"" ", pdb);
+            }
+
+            if (emitDocFile)
+            {
+                var doc = Path.Combine(outputPath, _project.Name + ".xml");
+
+                fscArgBuilder = fscArgBuilder
+                    .AppendFormat(@"--doc:""{0}"" ", doc);
+            }
+
+            // F# cares about order so assume that the files were listed in order
+            var fscArgs = fscArgBuilder.Append(string.Join(" ", _project.SourceFiles.Select(s => "\"" + s + "\"")))
+                                       .Append(" ");
+
+            var tempFiles = new List<string>();
+
+            // These are the metadata references being used by your project.
+            // Everything in your project.json is resolved and normailzed here:
+            // - Project references
+            // - Package references are turned into the appropriate assemblies
+            // - Assembly neutral references
+            // Each IMetadaReference maps to an assembly
+            foreach (var reference in _projectExport.MetadataReferences)
+            {
+                // Skip this project
+                if (reference.Name == typeof(FSharpProjectReference).Assembly.GetName().Name)
+                {
+                    continue;
+                }
+
+                // NuGet references
+                var fileReference = reference as IMetadataFileReference;
+                if (fileReference != null)
+                {
+                    fscArgs.AppendFormat(@"-r:""{0}""", fileReference.Path)
+                      .Append(" ");
+                }
+
+                // Assembly neutral references
+                var embeddedReference = reference as IMetadataEmbeddedReference;
+                if (embeddedReference != null)
+                {
+                    var tempEmbeddedPath = Path.Combine(tempBasePath, reference.Name + ".dll");
+
+                    // Write the ANI to disk for csc
+                    File.WriteAllBytes(tempEmbeddedPath, embeddedReference.Contents);
+
+                    fscArgs.AppendFormat(@"-r:""{0}""", tempEmbeddedPath)
+                      .Append(" ");
+
+                    tempFiles.Add(tempEmbeddedPath);
+                }
+
+                var projectReference = reference as IMetadataProjectReference;
+                if (projectReference != null)
+                {
+                    // You can write the reference assembly to the stream
+                    // and add the reference to your compiler
+
+                    var tempProjectDll = Path.Combine(tempBasePath, reference.Name + ".dll");
+
+                    using (var fs = File.OpenWrite(tempProjectDll))
+                    {
+                        projectReference.EmitReferenceAssembly(fs);
+                    }
+
+                    fscArgs.AppendFormat(@"-r:""{0}""", tempProjectDll)
+                      .Append(" ");
+
+                    tempFiles.Add(tempProjectDll);
+                }
+            }
+
+            // For debugging
+            // Console.WriteLine(fscArgs.ToString());
+
+            var si = new ProcessStartInfo
+            {
+                FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Microsoft SDKs\F#\3.1\Framework\v4.0\Fsc.exe"),
+                Arguments = fscArgs.ToString(),
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            var process = Process.Start(si);
+            process.WaitForExit();
+
+            var errors = new List<string>();
+            var warnings = new List<string>();
+
+            string line = null;
+            while ((line = process.StandardError.ReadLine()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                if (line.Contains(" warning "))
+                {
+                    warnings.Add(line);
+                }
+                else
+                {
+                    errors.Add(line);
+                }
+            }
+
+            if (process.ExitCode != 0)
+            {
+                return new ProjectBuildResult(success: false, warnings: warnings, errors: errors);
+            }
+
+            // Nuke the temporary references on disk
+            tempFiles.ForEach(File.Delete);
+
+            Directory.Delete(tempBasePath);
+
+            return new ProjectBuildResult(success: true, warnings: warnings, errors: errors);
         }
     }
 }
--- src/HelloWorldFSharp/Program.fs
+++ src/HelloWorldFSharp/Program.fs
@@ -1,5 +1,8 @@
-﻿[<EntryPoint>]
+﻿open System
+
+[<EntryPoint>]
 let main args =
     printfn "Arguments passed to function : %A" args
+    Console.ReadKey() |> ignore
     // Return 0. This indicates success.
     0
