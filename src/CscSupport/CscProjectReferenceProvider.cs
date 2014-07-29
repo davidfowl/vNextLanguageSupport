c1767fa46563c54ba5d3b8e6613b1a4b8a548a25
David Fowler <davidfowl@gmail.com>
2014-07-28 21:58:58 -0700

    Updated with the latest changes

---
+++ src/CscSupport/CscProjectReferenceProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using Microsoft.Framework.Runtime;
+
+namespace CscSupport
+{
+    public class CscProjectReferenceProvider : IProjectReferenceProvider
+    {
+        public IMetadataProjectReference GetProjectReference(Project project, FrameworkName targetFramework, string configuration, ILibraryExport projectExport)
+        {
+            return new CscProjectReference(project, targetFramework, configuration, projectExport);
+        }
+    }
+}
---
+++ src/FSharpSupport/FSharpProjectReferenceProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Versioning;
+using Microsoft.Framework.Runtime;
+
+namespace FSharpSupport
+{
+    public class FSharpProjectReferenceProvider : IProjectReferenceProvider
+    {
+        public IMetadataProjectReference GetProjectReference(Project project, FrameworkName targetFramework, string configuration, ILibraryExport projectExport)
+        {
+            // Represents the project reference
+            return new FSharpProjectReference(project, targetFramework, configuration, projectExport);
+        }
+    }
+}
